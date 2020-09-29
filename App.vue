5938afcfb41cc2c11337c2574a79a7bc78580b37
Phillip <63778595+Phillip-708@users.noreply.github.com>
2020-09-29 18:37:41 +0900

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-link to="/Questions">Questions</router-link>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+//import Questions from "@/components/Questions";
+
+export default {
+  name: 'App',
+
+/*  components: {
+    Questions
+  },
+
+ */
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ EachQuestion.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="eachQuestion">
+    <router-view />
+    <h1>{{ question }}</h1>
+    <p v-for="comment in comments" :key="comment">{{ comment.contents }}</p>
+    <textarea v-model="input" @keydown.enter="add"/>
+    <button @click="add">送信</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EachQuestion",
+  props: {
+    question: String
+  },
+  data() {
+    return {
+      input: "",
+      comments: [],
+    };
+  },
+  methods: {
+    add() {
+      if(this.input.length < 1) {
+        return;
+      }
+
+      this.comments.push({
+        contents: this.input,
+        index: this.num
+      });
+      this.input= "";
+      this.num++;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
